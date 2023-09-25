ce92cca2c01f671aad6e2916d2f3b082b2266be5
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-09-25 19:01:04 +0200

    server.js fix
    add: create 'tmp' folder when server started and delete when is shoutdown

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const app = require("./app.js");
+const fs = require("fs");
 require("dotenv").config();
 
 mongoose
@@ -11,9 +12,32 @@ mongoose
     console.log("Database connection successful");
     app.listen(3000, () => {
       console.log("Server running. Use our API on port: 3000");
+
+      // create tmp folder
+      if (!fs.existsSync("tmp")) {
+        console.log("tmp folder created");
+        fs.mkdirSync("tmp");
+      }
     });
   })
   .catch((err) => {
     console.error("Database connection error:", err);
     process.exit(1);
   });
+
+// signs on which tmp folder is deleted
+// ctrl + C
+process.on("SIGINT", tmpDelete);
+
+// delete tmp foder func
+function tmpDelete() {
+  try {
+    if (fs.existsSync("tmp")) {
+      fs.rmdirSync("tmp");
+      console.log("tmp folder deleted");
+    }
+  } catch (err) {
+    console.error("error to delete tmp folder:", err);
+  }
+  process.exit();
+}
